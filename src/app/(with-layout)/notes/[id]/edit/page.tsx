847f1ab2d96269bb3a984e65f59acc84c5ad23ee
unknown <muhammadkabeer2003@gmail.com>
2026-02-21 03:42:51 +0500

    Updated Metadata

--- src/app/(with-layout)/notes/[id]/edit/page.tsx
+++ src/app/(with-layout)/notes/[id]/edit/page.tsx
@@ -1,15 +1,7 @@
-import { Metadata } from "next";
+
 import EditNotePage from "@/page-components/notes/EditNotePage";
 
-export const metadata: Metadata = {
-  title: "Edit Note | Merge",
-  description: "Edit and update your note",
-  openGraph: {
-    title: "Edit Note | Merge",
-    description: "Edit and update your note",
-    type: "website",
-  },
-};
+
 
 export default function Page() {
   return <EditNotePage />;
