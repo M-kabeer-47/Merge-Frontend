e97460d073433e98800dabe767946b353cbe254d
unknown <muhammadkabeer2003@gmail.com>
2026-02-20 19:03:41 +0500

    Deleted MetaData

--- src/app/(with-layout)/notes/create/page.tsx
+++ src/app/(with-layout)/notes/create/page.tsx
@@ -1,16 +1,6 @@
-import { Metadata } from "next";
 import CreateNotePage from "@/page-components/notes/CreateNotePage";
 
-export const metadata: Metadata = {
-  title: "Create Note | Merge",
-  description: "Create a new note to organize your study materials",
-  keywords: ["create note", "new note", "study materials", "notes"],
-  openGraph: {
-    title: "Create Note | Merge",
-    description: "Create a new note to organize your study materials",
-    type: "website",
-  },
-};
+
 
 export default function Page() {
   return <CreateNotePage />;
